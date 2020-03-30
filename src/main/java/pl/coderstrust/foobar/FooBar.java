5b9694bc1c3cc775a2877de9cceaaa47acbd7754
mac <ms.czerwonka@gmail.com>
2020-03-30 23:00:55 +0200

    corrected FooBar class code

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -2,19 +2,22 @@ package pl.coderstrust.foobar;
 
 public class FooBar {
     public static void main(String[] args) {
-        for(int i = 0; i <= 100; i++) {
-            System.out.println(i);
-            if (i != 0) {
-                if (i % 3 == 0 || i % 5 == 0) {
-                    if (i % 3 == 0) {
-                        System.out.print("Foo");
-                    }
-                    if (i % 5 == 0) {
-                        System.out.print("Bar");
-                    }
-                    System.out.println();
-                }
+        int number = 100;
+        printFooBar(number);
+    }
+
+    public static void printFooBar(int number) {
+        StringBuilder message = new StringBuilder();
+        for (int i = 0; i <= number; i++) {
+            message.append(i);
+            if (i % 3 == 0) {
+                message.append("Foo");
             }
+            if (i % 5 == 0) {
+                message.append("Bar");
+            }
+            System.out.println(message);
+            message.setLength(0);
         }
     }
 }
