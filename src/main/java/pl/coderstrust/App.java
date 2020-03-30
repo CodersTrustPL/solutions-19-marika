0bf94d9aad4aba15b34ee8f85039265d6874be0e
mac <ms.czerwonka@gmail.com>
2020-03-30 16:40:03 +0200

    Added basic project structure. Added foobar package and FooBar class.

---
+++ src/main/java/pl/coderstrust/App.java
@@ -0,0 +1,13 @@
+package pl.coderstrust;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
