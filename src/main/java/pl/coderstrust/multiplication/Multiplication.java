a2d727a02dfffd0e112ca7f3519cf5f3d9517b19
mac <ms.czerwonka@gmail.com>
2020-04-01 18:46:59 +0200

    added multiplication package and Multiplication class

--- src/main/java/pl/coderstrust/multiplication/Multiplication.java
+++ src/main/java/pl/coderstrust/multiplication/Multiplication.java
@@ -1,2 +1,28 @@
+package pl.coderstrust.multiplication;
+
 public class Multiplication {
+    public static void main(String[] args) {
+        int size = 10;
+        printMultiplicationTable(size);
+    }
+
+    public static void printMultiplicationTable(int size) {
+        StringBuilder multiplicationTable = new StringBuilder();
+
+        for(int i = 1; i<=size; i++) {
+            multiplicationTable.append("\t").append(i);
+        }
+        multiplicationTable.append("\n");
+
+        for(int i = 1; i<=size; i++) {
+            multiplicationTable.append(i).append("\t");
+            int counter = 1;
+            while(counter<=size) {
+                multiplicationTable.append(i * counter).append("\t");
+                counter++;
+            }
+            multiplicationTable.append("\n");
+        }
+        System.out.println(multiplicationTable);
+    }
 }
