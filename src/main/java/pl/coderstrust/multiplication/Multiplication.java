4c32358f2d8dcca15aabcf0eb3f1990fc1b941be
mac <ms.czerwonka@gmail.com>
2020-04-02 00:18:15 +0200

    fixed spaces

--- src/main/java/pl/coderstrust/multiplication/Multiplication.java
+++ src/main/java/pl/coderstrust/multiplication/Multiplication.java
@@ -7,13 +7,13 @@ public class Multiplication {
     }
     public static void printMultiplicationTable(int size) {
         StringBuilder multiplicationTable = new StringBuilder();
-        for(int i = 1; i<=size; i++) {
+        for(int i = 1; i <= size; i++) {
             multiplicationTable.append("\t").append(i);
         }
         multiplicationTable.append("\n");
-        for(int i = 1; i<=size; i++) {
+        for(int i = 1; i <= size; i++) {
             multiplicationTable.append(i).append("\t");
-            for(int j = 1; j<=size; j++) {
+            for(int j = 1; j <= size; j++) {
                 multiplicationTable.append(j * i).append("\t");
             }
             multiplicationTable.append("\n");
