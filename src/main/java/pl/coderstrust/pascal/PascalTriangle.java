1f48b36756047159f95c4bad2c9f3cb47e353183
mac <ms.czerwonka@gmail.com>
2020-04-05 12:10:47 +0200

    added changes sugested after pull request

--- src/main/java/pl/coderstrust/pascal/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascal/PascalTriangle.java
@@ -6,26 +6,26 @@ public class PascalTriangle {
         printPascalTriangle(height);
     }
 
-    public static int calculateFactorial(int n) {
-        int factorial = 1;
-        while (n >= 1) {
-            factorial *= n;
-            n--;
-        }
-        return factorial;
-    }
-
     public static void printPascalTriangle(int height) {
         int number;
-        for (int n = 0; n < height; n++) {
-            for (int i = n; i < height; i++) {
-                System.out.printf("%2s", " ");
+        for (int i = 0; i < height; i++) {
+            for (int j = i; j < height; j++) {
+                System.out.printf("%2s", "");
             }
-            for (int j = 0; j <= n; j++) {
-                number = calculateFactorial(n) / (calculateFactorial(j) * calculateFactorial(n - j));
+            for (int j = 0; j <= i; j++) {
+                number = factorial(i) / (factorial(j) * factorial(i - j));
                 System.out.printf("%4d", number);
             }
             System.out.println();
         }
+    }
+
+    public static int factorial(int number) {
+        int result = 1;
+        while (number >= 1) {
+            result *= number;
+            number--;
+        }
+        return result;
     }
 }
