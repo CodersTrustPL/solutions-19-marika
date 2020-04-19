c36dd61e0ba1c15e77312bd2ea011af291fc39f4
mac <ms.czerwonka@gmail.com>
2020-04-19 21:06:04 +0200

    added changed in FooBar class and FooBarTest class after pull request

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -5,21 +5,21 @@ import java.util.List;
 
 public class FooBar {
     public static void main(String[] args) {
-        int number = 100;
+        int number = 15;
         List<String> result = createFooBarArray(number);
         System.out.println(result);
     }
 
     public static List<String> createFooBarArray(int number) {
         List<String> result = new ArrayList<>();
-        for (int i = 0; i <= number; i++) {
+        for (int i = 1; i <= number; i++) {
             String message = "";
             message += Integer.toString(i);
             if (i % 3 == 0) {
-                message += "Foo";
+                message += " Foo";
             }
             if (i % 5 == 0) {
-                message += "Bar";
+                message += " Bar";
             }
             result.add(message);
         }
--- src/test/java/pl/coderstrust/foobar/FooBarTest.java
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -7,48 +7,84 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static pl.coderstrust.foobar.FooBar.createFooBarArray;
+
 class FooBarTest {
 
     @Test
     public void shouldReturnEmptyList() {
+
         //given
         int number = -1;
         List<String> expected = Collections.emptyList();
+
         //when
-        List<String> result = FooBar.createFooBarArray(number);
+        List<String> result = createFooBarArray(number);
+
         //then
         Assertions.assertEquals(expected, result);
     }
 
     @Test
-    public void shouldReturnOneElementListAsExpected() {
+    public void shouldReturnEmptyListTo() {
+
         //given
         int number = 0;
-        List<String> expected = Collections.singletonList("0FooBar");
+        List<String> expected = Collections.emptyList();
+
         //when
-        List<String> result = FooBar.createFooBarArray(number);
+        List<String> result = createFooBarArray(number);
+
         //then
         Assertions.assertEquals(expected, result);
     }
 
     @Test
-    public void shouldReturnSixElementListAsExpected() {
+    public void shouldReturnFiveElementListAsExpected() {
+
         //given
         int number = 5;
-        List<String> expected = Arrays.asList("0FooBar", "1", "2", "3Foo", "4", "5Bar");
+        List<String> expected = Arrays.asList(
+                "1",
+                "2",
+                "3 Foo",
+                "4",
+                "5 Bar"
+        );
+
         //when
-        List<String> result = FooBar.createFooBarArray(number);
+        List<String> result = createFooBarArray(number);
+
         //then
         Assertions.assertEquals(expected, result);
     }
 
     @Test
-    public void shouldReturnElevenElementListAsExpected() {
+    public void shouldReturnFifteenElementListAsExpected() {
+
         //given
-        int number = 10;
-        List<String> expected = Arrays.asList("0FooBar", "1", "2", "3Foo", "4", "5Bar", "6Foo", "7", "8", "9Foo", "10Bar");
+        int number = 15;
+        List<String> expected = Arrays.asList(
+                "1",
+                "2",
+                "3 Foo",
+                "4",
+                "5 Bar",
+                "6 Foo",
+                "7",
+                "8",
+                "9 Foo",
+                "10 Bar",
+                "11",
+                "12 Foo",
+                "13",
+                "14",
+                "15 Foo Bar"
+        );
+
         //when
-        List<String> result = FooBar.createFooBarArray(number);
+        List<String> result = createFooBarArray(number);
+
         //then
         Assertions.assertEquals(expected, result);
     }
