7be1bc347fe990ca7b0cc7ae1b8e4d1dcf5e4192
mac <ms.czerwonka@gmail.com>
2020-04-24 12:31:15 +0200

    added extension in Christmas class, added test in ChristmasTest, after pill request

--- src/main/java/pl/coderstrust/christmas/Christmas.java
+++ src/main/java/pl/coderstrust/christmas/Christmas.java
@@ -6,37 +6,43 @@ import java.util.List;
 public class Christmas {
 
     public static void main(String[] args) {
-        int height = 10;
+        int height = 5;
         List<String> christmasTree = createChristmasTree(height);
-        printChristmasTreeIfSizeIsBiggerThanZero(height, christmasTree);
+        printChristmasTree(height, christmasTree);
+    }
+
+    private static void throwExceptionIfHeightIsSmallerThanOne(int height) {
+        if (height < 1) {
+            throw new IllegalArgumentException("The value must be greater than 0! Provided value was: " + height);
+        }
     }
 
     public static List<String> createChristmasTree(int height) {
         List<String> christmasTree = new ArrayList<>();
         for (int i = 0; i < height; i++) {
-            String treeElement = "";
+            StringBuilder treeElement = new StringBuilder();
             for (int j = 0; j < (height - i); j++) {
-                treeElement += " ";
+                treeElement.append(" ");
             }
             for (int j = 1; j <= (2 * i + 1); j++) {
-                treeElement += "*";
+                treeElement.append("*");
             }
-            christmasTree.add(treeElement);
+            christmasTree.add(treeElement.toString());
         }
-        String treeElement = "";
+        StringBuilder treeElement = new StringBuilder();
         for (int i = 1; i < height; i++) {
-            treeElement += " ";
+            treeElement.append(" ");
         }
-        treeElement += "**";
-        christmasTree.add(treeElement);
+        treeElement.append("**");
+        christmasTree.add(treeElement.toString());
+        System.out.println(christmasTree);
         return christmasTree;
     }
 
-    public static void printChristmasTreeIfSizeIsBiggerThanZero(int height, List<String> christmasTree) {
-        if (height > 0) {
-            for (int i = 0; i < christmasTree.size(); i++) {
-                System.out.println(christmasTree.get(i));
-            }
+    public static void printChristmasTree(int height, List<String> christmasTree) {
+        throwExceptionIfHeightIsSmallerThanOne(height);
+        for (int i = 0; i < christmasTree.size(); i++) {
+            System.out.println(christmasTree.get(i));
         }
     }
 }
--- src/test/java/pl/coderstrust/christmas/ChristmasTest.java
+++ src/test/java/pl/coderstrust/christmas/ChristmasTest.java
@@ -2,56 +2,60 @@ package pl.coderstrust.christmas;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static pl.coderstrust.christmas.Christmas.createChristmasTree;
+import static pl.coderstrust.christmas.Christmas.printChristmasTree;
 
 class ChristmasTest {
 
     @Test
-    public void shouldCheckIfAllValuesAreCorrectWhenHeightIsTwo() {
+    public void shouldCreateChristmasTreeWhenHeightIsTwo() {
         //given
         int height = 2;
-        List<String> expected = Arrays.asList(
+        String expected = Arrays.asList(
                 "  *",
                 " ***",
                 " **"
-        );
+        ).toString();
 
         //when
-        List<String> result = createChristmasTree(height);
+        String result = createChristmasTree(height).toString();
 
         //then
         assertEquals(expected, result);
     }
 
     @Test
-    public void shouldCheckIfAllValuesAreCorrectWhenHeightIsFive() {
+    public void shouldCreateChristmasTreeWhenHeightIsFive() {
         //given
         int height = 5;
-        List<String> expected = Arrays.asList(
+        String expected = Arrays.asList(
                 "     *",
                 "    ***",
                 "   *****",
                 "  *******",
                 " *********",
                 "    **"
-        );
+        ).toString();
 
         //when
-        List<String> result = createChristmasTree(height);
+        String result = createChristmasTree(height).toString();
 
         //then
         assertEquals(expected, result);
     }
 
     @Test
-    public void shouldCheckIfAllValuesAreCorrectWhenHeightIsTen() {
+    public void shouldCreateChristmasTreeWhenHeightIsTen() {
         //given
         int height = 10;
-        List<String> expected = Arrays.asList(
+        String expected = Arrays.asList(
                 "          *",
                 "         ***",
                 "        *****",
@@ -63,12 +67,29 @@ class ChristmasTest {
                 "  *****************",
                 " *******************",
                 "         **"
-        );
+        ).toString();
 
         //when
-        List<String> result = createChristmasTree(height);
+        String result = createChristmasTree(height).toString();
 
         //then
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void shouldCheckTfThrowExceptionWhenHeightIsSmallerThanOne() {
+        //given
+        int height = -1;
+        List<String> christmasTree = new ArrayList<String>();
+        String expected = "The value must be greater than 0!";
+
+        //when
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            printChristmasTree(height, christmasTree);
+        });
+
+        //then
+        String result = exception.getMessage();
+        assertTrue(result.contains(expected));
     }
 }
