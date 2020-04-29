17fd3eb30c3ed717441cb311c88cb9cf3adca870
mac <ms.czerwonka@gmail.com>
2020-04-29 16:53:56 +0200

    changed HanoiTowersclass and added hanoi text file after pull request

--- src/main/java/pl/coderstrust/hanoi/HanoiTowers.java
+++ src/main/java/pl/coderstrust/hanoi/HanoiTowers.java
@@ -1,54 +1,41 @@
 package pl.coderstrust.hanoi;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.Stack;
 
 public class HanoiTowers {
 
     public static void main(String[] args) throws IOException {
         int disks = 3;
-        List<String> pillarA = new ArrayList<String>();
-        pillarA.add("pillar A");
-        fillPillarOne(disks, pillarA);
-        List<String> pillarB = new ArrayList<String>();
-        pillarB.add("pillar B");
-        List<String> pillarC = new ArrayList<String>();
-        pillarC.add("pillar C");
+        Stack<String> pillarA = new Stack<String>();
+        Stack<String> pillarB = new Stack<String>();
+        Stack<String> pillarC = new Stack<String>();
+        writeToFile();
+        addNamesToPillar(pillarA, pillarB, pillarC);
+        addDisksToFirstPillar(disks, pillarA);
         hanoiTowers(disks, pillarA, pillarB, pillarC);
     }
 
-    public static void hanoiTowers(int disks, List<String> pillarA, List<String> pillarB, List<String> pillarC) throws IOException {
+    public static void hanoiTowers(int disks, Stack<String> pillarA, Stack<String> pillarB, Stack<String> pillarC) throws IOException {
         throwExceptionIfHeightIsSmallerThanOne(disks);
         if (disks == 1) {
-            String disk = pillarA.get(pillarA.size() -1);
-            pillarA.remove(disk);
-            pillarC.add(disk);
-            String message = "Move disk: " + disk + " from: " + pillarA.get(0) + " to: " + pillarC.get(0);
-            writeToFile(message, pillarA, pillarB, pillarC);
-            System.out.println(message);
-            System.out.println(pillarA);
-            System.out.println(pillarB);
-            System.out.println(pillarC);
+            String currentState = moveDisk(pillarA, pillarC);
+            printCurrentState(currentState, pillarA, pillarB, pillarC);
             return;
         }
         hanoiTowers(disks - 1, pillarA, pillarC, pillarB);
-        String disk = pillarA.get(pillarA.size() -1);
-        pillarA.remove(disk);
-        pillarC.add(disk);
-        String message = "Move disk: " + disk + " from: " + pillarA.get(0) + " to: " + pillarC.get(0);
-        writeToFile(message, pillarA, pillarB, pillarC);
-        System.out.println(message);
-        System.out.println(pillarA);
-        System.out.println(pillarB);
-        System.out.println(pillarC);
+        String currentState = moveDisk(pillarA, pillarC);
+        printCurrentState(currentState, pillarA, pillarB, pillarC);
         hanoiTowers(disks - 1, pillarB, pillarA, pillarC);
     }
 
-    private static void fillPillarOne(int disks, List<String> pillar) {
+    private static void addNamesToPillar(Stack<String> pillarA, Stack<String> pillarB, Stack<String> pillarC) {
+        pillarA.add("pillar A");
+        pillarB.add("pillar B");
+        pillarC.add("pillar C");
+    }
+
+    private static void addDisksToFirstPillar(int disks, Stack<String> pillar) {
         for (int i = 0; i < disks; i++) {
             pillar.add(Integer.toString(i + 1));
         }
@@ -60,22 +47,21 @@ public class HanoiTowers {
         }
     }
 
-    private static void writeToFile(String message, List<String> pillarA, List<String> pillarB, List<String> pillarC
-                                    ) throws IOException {
-        File myFile = new File("text.txt");
-        myFile.createNewFile();
-        FileWriter myWriter = new FileWriter(myFile.getAbsoluteFile(), true);
-        BufferedWriter myBufferedWriter = new BufferedWriter(myWriter);
-        myBufferedWriter.write(message);
-        myBufferedWriter.newLine();
-        myBufferedWriter.write(pillarA.toString());
-        myBufferedWriter.newLine();
-        myBufferedWriter.write(pillarB.toString());
-        myBufferedWriter.newLine();
-        myBufferedWriter.write(pillarC.toString());
-        myBufferedWriter.newLine();
-        myBufferedWriter.close();
+    private static String moveDisk(Stack<String> pillarA, Stack<String> pillarC) {
+        String disk = pillarA.pop();
+        pillarC.add(disk);
+        return "Move disk: " + disk + " from: " + pillarA.get(0) + " to: " + pillarC.get(0);
+    }
+
+    private static void printCurrentState(String message, Stack<String> pillarA, Stack<String> pillarB, Stack<String> pillarC) {
+        System.out.println(message);
+        System.out.println(pillarA);
+        System.out.println(pillarB);
+        System.out.println(pillarC);
+    }
+
+    private static void writeToFile() throws IOException {
+        PrintStream out = new PrintStream("hanoi.txt");
+        System.setOut(out);
     }
 }
-
-
