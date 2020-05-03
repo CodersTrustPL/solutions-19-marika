d96132cf437a7a95efdb135133a8309761a9ab65
mac <ms.czerwonka@gmail.com>
2020-05-03 18:42:18 +0200

    refactored FibonacciChecker class and added new test - random values

--- src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
@@ -11,21 +11,21 @@ import java.util.Set;
 public class FibonacciChecker {
 
     public static void main(String[] args) throws IOException {
-        long number = 5;
+        long number = 6;
         System.out.println(isFibonacciNumber(number));
     }
 
-    public static Set<Long> fibonacciListFromFile(String fileName) throws IOException {
-        List<String> fibonacciList = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+    public static boolean isFibonacciNumber(long number) throws IOException {
+        Set<Long> fibonacciNumbers = fibonacciNumbersFromFile();
+        return fibonacciNumbers.contains(number);
+    }
+
+    private static Set<Long> fibonacciNumbersFromFile() throws IOException {
+        List<String> fibonacciList = Files.readAllLines(Paths.get("src/test/resources/fibonacci.csv"), StandardCharsets.UTF_8);
         Set<Long> fibonacciNumbers = new HashSet<>();
         for(String item: fibonacciList) {
             fibonacciNumbers.add(Long.parseLong(item));
         }
         return fibonacciNumbers;
-    }
-
-    public static boolean isFibonacciNumber(long number) throws IOException {
-        Set<Long> fibonacciNumbers = fibonacciListFromFile("src/test/resources/fibonacci.csv");
-        return fibonacciNumbers.contains(number);
     }
 }
--- src/test/java/pl/coderstrust/fibonacci/FibonacciCheckerTest.java
+++ src/test/java/pl/coderstrust/fibonacci/FibonacciCheckerTest.java
@@ -2,17 +2,49 @@ package pl.coderstrust.fibonacci;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FibonacciCheckerTest {
-    
+
     @ParameterizedTest
     @CsvFileSource(resources = "/fibonacci.csv", numLinesToSkip = 1)
     void shouldReturnsTrueForEachNumberFromFibonacciSet(String input) throws IOException {
         Long actualValue = Long.parseLong(input);
         assertTrue(FibonacciChecker.isFibonacciNumber(actualValue));
+    }
+
+    @ParameterizedTest
+    @MethodSource("randomNumbers")
+    void shouldReturnFalseForAllRandomNumbers(long actualValue) throws IOException {
+        assertFalse(FibonacciChecker.isFibonacciNumber(actualValue));
+    }
+
+    private static Stream<Long> randomNumbers() throws IOException {
+        Set<Long> randomNumbers = new HashSet<>();
+        Random randomGenerator = new Random();
+        for (int i = 0; i < 1000000; i++) {
+            randomNumbers.add(randomGenerator.nextLong());
+        }
+        List<String> fibonacciList = Files.readAllLines(Paths.get("src/test/resources/fibonacci.csv"),
+                StandardCharsets.UTF_8);
+        Set<Long> fibonacciNumbers = new HashSet<>();
+        for(String item: fibonacciList) {
+            fibonacciNumbers.add(Long.parseLong(item));
+        }
+        randomNumbers.removeAll(fibonacciNumbers);
+        return randomNumbers.stream();
     }
 }
