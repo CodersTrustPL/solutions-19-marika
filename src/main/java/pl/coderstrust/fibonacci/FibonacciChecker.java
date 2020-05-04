6ff26aec6932cc0c6d79b1a16d35cdf2c4a97fe5
mac <ms.czerwonka@gmail.com>
2020-05-04 17:41:24 +0200

    create FibonacciNumbersGenerator class and FibonacciChecker class

--- src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
@@ -1,9 +1,10 @@
 package pl.coderstrust.fibonacci;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class FibonacciChecker {
+
+    static Set<Long> fibonacciNumbers = FibonacciNumbersGenerator.generateFibonacciNumbers();
 
     public static void main(String[] args) {
         long number = 10;
@@ -12,23 +13,7 @@ public class FibonacciChecker {
     }
 
     public static boolean isFibonacciNumber(long number) {
-        Set<Long> fibonacciNumbers = fibonacciNumbersGenerator();
         return fibonacciNumbers.contains(number);
-    }
-
-    public static Set<Long> fibonacciNumbersGenerator() {
-        Set<Long> fibonacciNumbers = new HashSet<>();
-        long first = 0;
-        fibonacciNumbers.add(first);
-        long second = 0;
-        long sum = 1;
-        for (int i = 1; i < 92; i++) {
-            first = second;
-            second = sum;
-            sum = first + second;
-            fibonacciNumbers.add(sum);
-        }
-        return fibonacciNumbers;
     }
 
     private static void throwExceptionIfHeightIsSmallerThanZero(long number) {
