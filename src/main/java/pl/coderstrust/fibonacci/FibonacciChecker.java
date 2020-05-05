7749a6d0f00baf589aef5f18ec18396f8e3ee112
mac <ms.czerwonka@gmail.com>
2020-05-05 22:34:10 +0200

    reformat code after checkstyle

--- src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
@@ -5,34 +5,33 @@ import java.util.Set;
 
 public class FibonacciChecker {
 
-    static Set<Long> fibonacciNumbers = generateFibonacciNumbers();
+  static Set<Long> fibonacciNumbers = generateFibonacciNumbers();
 
-    public static void main(String[] args) {
-        long number = 2;
-        System.out.println(isFibonacciNumber(number));
+  public static void main(String[] args) {
+    long number = 12;
+    System.out.println(isFibonacciNumber(number));
+  }
+
+  public static boolean isFibonacciNumber(long number) {
+    return fibonacciNumbers.contains(number);
+  }
+
+  private static Set<Long> generateFibonacciNumbers() {
+    Set<Long> fibonacciNumbers = new HashSet<>();
+    long first = 0;
+    fibonacciNumbers.add(first);
+    long second = 0;
+    long sum = 1;
+    for (int i = 1; i < 92; i++) {
+      first = second;
+      second = sum;
+      sum = first + second;
+      fibonacciNumbers.add(sum);
     }
+    return fibonacciNumbers;
+  }
 
-    public static boolean isFibonacciNumber(long number) {
-        return fibonacciNumbers.contains(number);
-    }
-
-    private static Set<Long> generateFibonacciNumbers() {
-        Set<Long> fibonacciNumbers = new HashSet<>();
-        long first = 0;
-        fibonacciNumbers.add(first);
-        long second = 0;
-        long sum = 1;
-        for (int i = 1; i < 92; i++) {
-            first = second;
-            second = sum;
-            sum = first + second;
-            fibonacciNumbers.add(sum);
-        }
-        return fibonacciNumbers;
-    }
-
-    public static Set<Long> getFibonacciNumbers() {
-        return fibonacciNumbers;
-
-    }
+  public static Set<Long> getFibonacciNumbers() {
+    return fibonacciNumbers;
+  }
 }
--- src/test/java/pl/coderstrust/fibonacci/FibonacciCheckerTest.java
+++ src/test/java/pl/coderstrust/fibonacci/FibonacciCheckerTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static pl.coderstrust.fibonacci.FibonacciChecker.getFibonacciNumbers;
 
-
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -15,27 +14,27 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class FibonacciCheckerTest {
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/fibonacci.csv")
-    void shouldReturnsTrueForEachNumberFromFibonacciSet(String input) {
-        long actualValue = Long.parseLong(input);
-        assertTrue(FibonacciChecker.isFibonacciNumber(actualValue));
-    }
+  @ParameterizedTest
+  @CsvFileSource(resources = "/fibonacci.csv")
+  void shouldReturnsTrueForEachNumberFromFibonacciSet(String input) {
+    long actualValue = Long.parseLong(input);
+    assertTrue(FibonacciChecker.isFibonacciNumber(actualValue));
+  }
 
-    @ParameterizedTest
-    @MethodSource("randomNumbers")
-    void shouldReturnFalseForAllRandomNumbers(long actualValue) {
-        assertFalse(FibonacciChecker.isFibonacciNumber(actualValue));
-    }
+  @ParameterizedTest
+  @MethodSource("randomNumbers")
+  void shouldReturnFalseForAllRandomNumbers(long actualValue) {
+    assertFalse(FibonacciChecker.isFibonacciNumber(actualValue));
+  }
 
-    private static Stream<Long> randomNumbers() {
-        Set<Long> randomNumbers = new HashSet<>();
-        Random randomGenerator = new Random();
-        for (int i = 0; i < 100; i++) {
-            randomNumbers.add(randomGenerator.nextLong());
-        }
-        Set<Long> fibonacciNumbers = getFibonacciNumbers();
-        randomNumbers.removeAll(fibonacciNumbers);
-        return randomNumbers.stream();
+  private static Stream<Long> randomNumbers() {
+    Set<Long> randomNumbers = new HashSet<>();
+    Random randomGenerator = new Random();
+    for (int i = 0; i < 100; i++) {
+      randomNumbers.add(randomGenerator.nextLong());
     }
+    Set<Long> fibonacciNumbers = getFibonacciNumbers();
+    randomNumbers.removeAll(fibonacciNumbers);
+    return randomNumbers.stream();
+  }
 }
